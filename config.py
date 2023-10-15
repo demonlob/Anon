ae029f854f345c6f0762c0fde8f91ebe67dba768
demonlob <142598456+demonlob@users.noreply.github.com>
2023-10-15 11:12:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org//file/d3473a6d02db397fc098a.jpg"
+    "START_IMG_URL", "https://graph.org//file/45076b458075ac4e46ab8.png"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org//file/d3473a6d02db397fc098a.jpg"
+    "PING_IMG_URL", "https://graph.org//file/477843143ba5136bbd87e.png"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
@@ -89,7 +89,7 @@ TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/0d10cc79e294c1e1ff026.jpg"
+YOUTUBE_IMG_URL = "https://graph.org//file/477843143ba5136bbd87e.png"
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"

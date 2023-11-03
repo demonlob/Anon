f688fa94ccae427a2dfb93c4ef74c8d328504664
demonlob <142598456+demonlob@users.noreply.github.com>
2023-11-03 21:47:29 +0530

    Update reload.py

--- AnonXMusic/plugins/tools/reload.py
+++ AnonXMusic/plugins/tools/reload.py
@@ -87,6 +87,7 @@ async def restartbot(client, message: Message, _):
 
 
 @app.on_callback_query(filters.regex("close") & ~BANNED_USERS)
+@ActualAdminCB
 async def close_menu(_, CallbackQuery):
     try:
         await CallbackQuery.answer()
